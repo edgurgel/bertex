77a7d37174a491bc6a70d7fd9cfde3b2e81c8bf1
Eduardo Gurgel <eduardo@gurgel.me>
2014-08-11 19:52:32 +1200

    Ensure support on Elixir 0.15.0 and OTP 17.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bertex.Mixfile do
   def project do
     [ app: :bertex,
       version: "1.2.0",
-      elixir: "~> 0.14.2",
+      elixir: "~> 0.14.2 or ~> 0.15.0",
       deps: [] ]
   end
 
