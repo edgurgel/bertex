fa0030e7ccd9b2972533a50f797d9f2e7a27e0fa
Eduardo Gurgel <eduardo@gurgel.me>
2014-08-23 13:57:01 +1200

    Add hex package definition
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -1,14 +1,25 @@
 defmodule Bertex.Mixfile do
   use Mix.Project
 
+  @description """
+    Elixir BERT encoder/decoder
+  """
+
   def project do
     [ app: :bertex,
       version: "1.2.0",
       elixir: "~> 0.14.2 or ~> 0.15.0",
+      name: "Bertex",
+      description: @description,
+      package: package,
       deps: [] ]
   end
 
-  def application do
-    []
+  def application, do: []
+
+  defp package do
+    [ contributors: ["Eduardo Gurgel Pinho"],
+      licenses: ["MIT"],
+      links: [ { "Github", "https://github.com/edgurgel/bertex" } ] ]
   end
 end
